c266d6e1fd2894c355a27a00317aebb347bfe9f3
Max Michaels <max32268@gmail.com>
2024-07-12 18:21:37 -0500

    WHMCS SSO

--- LoginController.php
+++ LoginController.php
@@ -2,30 +2,46 @@
 
 namespace Pterodactyl\Http\Controllers\Auth;
 
+use Ramsey\Uuid\Uuid;
 use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
+use Pterodactyl\Services\Users\UserCreationService;
 use Illuminate\Http\Request;
-use Pterodactyl\Models\User;
+use Illuminate\Auth\AuthManager;
 use Illuminate\Http\JsonResponse;
-use Pterodactyl\Facades\Activity;
 use Illuminate\Contracts\View\View;
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\View\Factory as ViewFactory;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
+use Laravel\Socialite\Facades\Socialite;
+use Pterodactyl\Contracts\Repository\UserRepositoryInterface;
+use Pterodactyl\Exceptions\Repository\RecordNotFoundException;
+use Pterodactyl\Models\User;
 
 class LoginController extends AbstractLoginController
 {
+    private ViewFactory $view;
+    
+    private $creationService;
+
     /**
      * LoginController constructor.
      */
-    public function __construct(private ViewFactory $view)
-    {
+    public function __construct(ViewFactory $view, UserCreationService $creationService) {
         parent::__construct();
+
+        $this->view = $view;
+        $this->creationService = $creationService;
     }
 
     /**
      * Handle all incoming requests for the authentication routes and render the
-     * base authentication view component. React will take over at this point and
-     * turn the login area into an SPA.
+     * base authentication view component. Vuejs will take over at this point and
+     * turn the login area into a SPA.
+     *
+     * @return \Illuminate\Contracts\View\View
      */
     public function index(): View
     {
@@ -34,6 +50,8 @@ class LoginController extends AbstractLoginController
 
     /**
      * Handle a login request to the application.
+     *
+     * @return \Illuminate\Http\JsonResponse|void
      *
      * @throws \Pterodactyl\Exceptions\DisplayException
      * @throws \Illuminate\Validation\ValidationException
@@ -50,35 +68,85 @@ class LoginController extends AbstractLoginController
 
             /** @var \Pterodactyl\Models\User $user */
             $user = User::query()->where($this->getField($username), $username)->firstOrFail();
-        } catch (ModelNotFoundException) {
+        } catch (ModelNotFoundException $exception) {
             $this->sendFailedLoginResponse($request);
         }
 
         // Ensure that the account is using a valid username and password before trying to
         // continue. Previously this was handled in the 2FA checkpoint, however that has
         // a flaw in which you can discover if an account exists simply by seeing if you
-        // can proceed to the next step in the login process.
+        // can proceede to the next step in the login process.
         if (!password_verify($request->input('password'), $user->password)) {
             $this->sendFailedLoginResponse($request, $user);
         }
 
-        if (!$user->use_totp) {
-            return $this->sendLoginResponse($user, $request);
+        if ($user->use_totp) {
+            $token = Str::random(64);
+
+            $request->session()->put('auth_confirmation_token', [
+                'user_id' => $user->id,
+                'token_value' => $token,
+                'expires_at' => CarbonImmutable::now()->addMinutes(5),
+            ]);
+
+            return new JsonResponse([
+                'data' => [
+                    'complete' => false,
+                    'confirmation_token' => $token,
+                ],
+            ]);
         }
 
-        Activity::event('auth:checkpoint')->withRequestMetadata()->subject($user)->log();
+        $this->auth->guard()->login($user, true);
 
-        $request->session()->put('auth_confirmation_token', [
-            'user_id' => $user->id,
-            'token_value' => $token = Str::random(64),
-            'expires_at' => CarbonImmutable::now()->addMinutes(5),
-        ]);
+        return $this->sendLoginResponse($user, $request);
+    }
 
-        return new JsonResponse([
-            'data' => [
-                'complete' => false,
-                'confirmation_token' => $token,
-            ],
+    public function redirectToProvider($provider): JsonResponse
+    {
+        return new JsonResponse(['redirect' => Socialite::driver($provider)->setScopes(['openid', 'email', 'profile'])->redirect()->getTargetUrl()]);
+    }
+
+    public function handleProviderCallback(Request $request, $provider)
+    {
+        try {
+            $user = Socialite::driver($provider)->user();
+        } catch (\Exception $e) {
+	    logger()->error($e);
+            return Redirect::route('auth.login');
+        }
+
+        $authUser = $this->findOrCreateUser($user);
+
+        if (! $authUser) {
+            return Redirect::route('auth.login');
+        }
+
+        Auth::login($authUser, true);
+
+        return Redirect::route('index');
+    }
+
+    protected function findOrCreateUser($oauthUser)
+    {
+        if ($authUser = User::query()->where('whmcs_user_token', $oauthUser->id)->first()) {
+            return $authUser;
+        }
+
+        if($authUser = User::query()->where('email', $oauthUser->email)->first()) {
+            $authUser->update([
+                'whmcs_user_token' => $oauthUser->id
+            ]);
+
+            return $authUser;
+        }
+
+        return $this->creationService->handle([
+            'username' => Str::random(8),
+            'email' => $oauthUser->email,
+            'name_first' => $oauthUser->name_first,
+            'name_last' => $oauthUser->name_last,
+            'whmcs_user_token' => $oauthUser->id,
         ]);
     }
 }
